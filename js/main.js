ba513dca94c9ebc5b49c95630468a25df3d52d39
uzoway <victor14944.ov@gmail.com>
2021-08-06 00:13:27 +0300

    Appended HTML markup to js file

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ function FetchMovie(e){
 
     let movieInput = document.getElementById("movieInput").nodeValue;
 
+    let movieDetails = "";
     // Fetch movie details from OMDB API
     fetch('http://www.omdbapi.com/?i=tt3896198&apikey=25e8a37f' + "&t+" + movieInput, {
         method: 'GET',
@@ -14,5 +15,25 @@ function FetchMovie(e){
         }
     })
     .then((res) => res.json())
-    .then((data) => console.log(data))
+    .then((data) => {
+
+        movieDetails += `
+            <div class="movie-header">
+            <h3></h3>
+            <button id="detailBtn">Details</button>
+            </div>
+            <div class="movie-image">
+                <img src="" alt="Movie Poster">
+            </div>
+            <div class="movie-details">
+                <p>Actors: <a href="#"></a></p>
+                <p>Director: <a href="#"></a></p>
+                <p>Genre: <a href="#"></a></p>
+                <p>Runtime: <a href="#"></a></p>
+                <p>Released: <a href="#"></a></p>
+                <p>Language: <a href="#"></a></p>
+            </div>
+        `
+        document.getElementById("movieBox").innerHTML = movieDetails;
+    })
 }
